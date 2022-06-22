b0cf2447a2c24de303c07efa98e7faee679a0bb5
Roman <ipromanone@gmail.com>
2022-06-22 21:55:34 +0300

    Added random values ​​for the square

--- index.js
+++ index.js
@@ -25,15 +25,23 @@ function handleBoxClick(event) {
 function renderBox() {
     $game.innerHTML = '';
     let box = document.createElement('div');
+    let boxSize = getRandom(30,100);
+    let gameSize = $game.getBoundingClientRect();
+    let maxTop = gameSize.height - boxSize;
+    let maxLeft = gameSize.width - boxSize;
 
-    box.style.height = box.style.width = '50px';
+    box.style.height = box.style.width = boxSize + 'px';
     box.style.position = 'absolute';
     box.style.backgroundColor = '#000';
-    box.style.top = '50px';
-    box.style.left = '70px';
+    box.style.top = getRandom(0, maxTop) + 'px';
+    box.style.left = getRandom(0, maxLeft) + 'px';
     box.style.cursor = 'pointer';
     box.setAttribute('data-box', true)
 
 
-    game.insertAdjacentElement('afterbegin', box);
+    $game.insertAdjacentElement('afterbegin', box);
+}
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min) 
 }
