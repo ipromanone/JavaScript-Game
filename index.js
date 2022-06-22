a0ccc3e750650508b29021f9d58c2ace04002227
Roman <ipromanone@gmail.com>
2022-06-22 21:39:59 +0300

    create and add square. Add dynamics

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const $start = document.querySelector('#start');
-const $game = document.querySelector('#game');
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+
+let score = 0;
 
 
 
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
     $game.style.backgroundColor = '#fff';
@@ -12,6 +15,25 @@ function startGame() {
     renderBox();
 }
 
-function renderBox() {
+function handleBoxClick(event) {
+    if (event.target.dataset.box) {
+        score++;
+        renderBox()
+    }
+}
 
+function renderBox() {
+    $game.innerHTML = '';
+    let box = document.createElement('div');
+
+    box.style.height = box.style.width = '50px';
+    box.style.position = 'absolute';
+    box.style.backgroundColor = '#000';
+    box.style.top = '50px';
+    box.style.left = '70px';
+    box.style.cursor = 'pointer';
+    box.setAttribute('data-box', true)
+
+
+    game.insertAdjacentElement('afterbegin', box);
 }
